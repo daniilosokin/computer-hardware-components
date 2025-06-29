4cff9886e887a688aafdefc5350a4c7e21eccfee
oryol <d.d.osokin@mail.ru>
2025-06-29 14:25:51 +0400

    Renamed variable, added spacings to #include section

--- app_b/main.cpp
+++ app_b/main.cpp
@@ -1,4 +1,5 @@
 #include "main_window.hpp"
+
 #include <QApplication>
 
 int main(int argc, char *argv[])
--- app_c/main.cpp
+++ app_c/main.cpp
@@ -1,10 +1,11 @@
 #include "main_window.hpp"
+
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QApplication b(argc, argv);
     MainWindow w;
     w.show();
-    return a.exec();
+    return b.exec();
 }
