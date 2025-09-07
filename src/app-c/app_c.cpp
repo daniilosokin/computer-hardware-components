00d30c90f442b53dc74946d79688a36944b18627
daniilosokin <d.d.osokin@mail.ru>
2025-09-08 00:28:40 +0400

    refactor: renamed from main.cpp

---
+++ src/app-c/app_c.cpp
@@ -0,0 +1,11 @@
+﻿#include "app_c_window.hpp"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication app_c(argc, argv);
+    AppAWindow window_app_c;
+    window_app_c.show();
+    return app_c.exec();
+}
