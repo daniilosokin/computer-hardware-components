94670117a3e9c6b300b3af3fd78044d29bcd8e2a
daniilosokin <d.d.osokin@mail.ru>
2025-10-06 01:17:23 +0400

    feat: added IPC through Qt's shared memory

---
+++ include/shared_data/shared_data.cpp
@@ -0,0 +1,68 @@
+#include "shared_data.hpp"
+
+#include <QMessageBox>
+
+SharedMemoryManager::SharedMemoryManager()
+	: shared_mem_(kSharedMemName),
+	semaphore_(kSemaphoreName, 1),
+	is_attached_(false)
+{}
+
+SharedMemoryManager::~SharedMemoryManager() 
+{
+	if (is_attached_) 
+		shared_mem_.detach();
+}
+
+bool SharedMemoryManager::Initialize() 
+{
+	Lock();
+
+	if (!shared_mem_.create(sizeof(SharedData)) 
+		&& shared_mem_.error() == QSharedMemory::AlreadyExists) 
+	{
+		if (!shared_mem_.attach()) 
+		{
+			Unlock();
+			return false;
+		}
+	} 
+	else if (!shared_mem_.isAttached()) 
+	{
+		Unlock();
+		return false;
+	}
+
+	is_attached_ = true;
+
+	if (shared_mem_.size() == sizeof(SharedData)) 
+	{
+		SharedData* data = static_cast<SharedData*>(shared_mem_.data());
+		data->period_t      = 1000;
+		data->beep_enabled  = false;
+		data->app_b_running = false;
+		data->app_c_running = false;
+		data->terminate_all = false;
+	}
+
+	Unlock();
+	return true;
+}
+
+void SharedMemoryManager::Lock() 
+{
+	semaphore_.acquire();
+}
+
+void SharedMemoryManager::Unlock() 
+{
+	semaphore_.release();
+}
+
+SharedData* SharedMemoryManager::GetData()
+{
+	if (!is_attached_ || !shared_mem_.data()) 
+		return nullptr;
+
+	return static_cast<SharedData*>(shared_mem_.data());
+}
---
+++ include/shared_data/shared_data.hpp
@@ -0,0 +1,36 @@
+#ifndef LIB_SHARED_DATA_HPP_
+#define LIB_SHARED_DATA_HPP_
+
+#include <QSharedMemory>
+#include <QSystemSemaphore>
+
+struct SharedData 
+{
+	int period_t;
+	bool beep_enabled;
+	bool app_b_running;
+	bool app_c_running;
+	bool terminate_all;
+};
+
+constexpr char kSharedMemName[] = "MultiAppSharedMemory";
+constexpr char kSemaphoreName[] = "MultiAppSemaphore";
+
+class SharedMemoryManager 
+{
+public:
+	SharedMemoryManager();
+	~SharedMemoryManager();
+
+	bool Initialize();
+	void Lock();
+	void Unlock();
+	SharedData* GetData();
+
+private:
+	QSharedMemory shared_mem_;
+	QSystemSemaphore semaphore_;
+	bool is_attached_;
+};
+
+#endif  // LIB_SHARED_DATA_HPP_
